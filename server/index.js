599dc30dd4b7281aeb11b92c6af5c24be6a52a47
Ruveyda <ruveyday20@gmail.com>
2022-12-06 23:42:11 +0300

    add: 🧷 client and server folder

---
+++ server/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const routes = require('./routes/index');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/', routes);
+
+const port = process.env.PORT || 5001;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
